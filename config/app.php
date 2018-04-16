a7b54433d6f557457a85a8faba66f178b3d89a6a
noyola <metahuargen@gmail.com>
2018-04-16 01:58:00 -0600

    cambios finalesv3

--- config/app.php
+++ config/app.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -12,9 +10,7 @@ return [
     | services your application utilizes. Set this in your ".env" file.
     |
     */
-
     'env' => env('APP_ENV', 'production'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
@@ -25,9 +21,7 @@ return [
     | application. If disabled, a simple generic error page is shown.
     |
     */
-
-    'debug' => env('APP_DEBUG', false),
-
+    'debug' => env('APP_DEBUG', 'forge'),
     /*
     |--------------------------------------------------------------------------
     | Application URL
@@ -38,9 +32,7 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-
     'url' => env('APP_URL', 'http://localhost'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
@@ -51,9 +43,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
     'timezone' => 'America/El_Salvador',
-
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
@@ -64,9 +54,7 @@ return [
     | to any of the locales which will be supported by the application.
     |
     */
-
     'locale' => 'es',
-
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -77,9 +65,7 @@ return [
     | the language folders that are provided through your application.
     |
     */
-
     'fallback_locale' => 'es',
-
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
@@ -90,11 +76,8 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
-
-    'key' => env('APP_KEY'),
-
+    'key' => env('APP_KEY','forge'),
     'cipher' => 'AES-256-CBC',
-
     /*
     |--------------------------------------------------------------------------
     | Logging Configuration
@@ -107,9 +90,7 @@ return [
     | Available Settings: "single", "daily", "syslog", "errorlog"
     |
     */
-
     'log' => env('APP_LOG', 'single'),
-
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
@@ -120,15 +101,10 @@ return [
     | this array to grant expanded functionality to your applications.
     |
     */
-
     'providers' => [
-
         /*
          * Laravel Framework Service Providers...
          */
-
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -150,10 +126,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-        Laracasts\Flash\FlashServiceProvider::class,
-
-
         /*
          * Application Service Providers...
          */
@@ -161,9 +133,10 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+		Barryvdh\DomPDF\ServiceProvider::class,
+		Collective\Html\HtmlServiceProvider::class,
+		Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
@@ -174,9 +147,7 @@ return [
     | the aliases are "lazy" loaded so they don't hinder performance.
     |
     */
-
     'aliases' => [
-        'PDF' => Barryvdh\DomPDF\Facade::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
@@ -206,12 +177,10 @@ return [
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,          
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-
-
+        'View' => Illuminate\Support\Facades\View::class,
+		'Form' => Collective\Html\FormFacade::class,
+		'PDF' => Barryvdh\DomPDF\Facade::class,
+		'Html' => Collective\Html\HtmlFacade::class,
+		'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
-
 ];
--- config/mail.php
+++ config/mail.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Mail Driver
@@ -15,9 +13,7 @@ return [
     |            "ses", "sparkpost", "log"
     |
     */
-
     'driver' => env('MAIL_DRIVER', 'smtp'),
-
     /*
     |--------------------------------------------------------------------------
     | SMTP Host Address
@@ -28,9 +24,7 @@ return [
     | the Mailgun mail service which will provide reliable deliveries.
     |
     */
-
-    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
     /*
     |--------------------------------------------------------------------------
     | SMTP Host Port
@@ -41,9 +35,7 @@ return [
     | stay compatible with the Mailgun e-mail application by default.
     |
     */
-
-    'port' => env('MAIL_PORT', 465),
-
+    'port' => env('MAIL_PORT', 587),
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
@@ -54,9 +46,7 @@ return [
     | used globally for all e-mails that are sent by your application.
     |
     */
-
-    'from' => ['address' => 'siarcaf@gmail.com', 'name' => 'agu ues'],
-
+    'from' => ['address' => null, 'name' => null],
     /*
     |--------------------------------------------------------------------------
     | E-Mail Encryption Protocol
@@ -67,9 +57,7 @@ return [
     | transport layer security protocol should provide great security.
     |
     */
-
-    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
-
+    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
     /*
     |--------------------------------------------------------------------------
     | SMTP Server Username
@@ -80,9 +68,7 @@ return [
     | connection. You may also set the "password" value below this one.
     |
     */
-
     'username' => env('MAIL_USERNAME'),
-
     /*
     |--------------------------------------------------------------------------
     | SMTP Server Password
@@ -93,9 +79,7 @@ return [
     | connection so that the application will be able to send messages.
     |
     */
-
     'password' => env('MAIL_PASSWORD'),
-
     /*
     |--------------------------------------------------------------------------
     | Sendmail System Path
@@ -106,10 +90,7 @@ return [
     | been provided here, which will work well on most of your systems.
     |
     */
-
     'sendmail' => '/usr/sbin/sendmail -bs',
-
-
     'stream' => [
         'ssl' => [
             'allow_self_signed' => true,
@@ -117,5 +98,4 @@ return [
             'verify_peer_name' => false,
         ],
     ],
-
 ];
